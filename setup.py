c12839859d24c505348de275ddfad5310a96d1fb
sathish <sathishsathi500@gmail.com>
2019-03-21 07:22:33 +0000

    package added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kafka_splunk_connector-sathishsathi500@gmail.com",
+    version="0.0.1",
+    author="Sathish",
+    author_email="sathishsathi500@gmail.com",
+    description="Library that connects the kafka topic to splunk",
+    long_description=long_description,
+    install_requires=[
+	'kafka-python',
+	'requests',
+	'logging',
+	'splunk_hec_handler'
+    ]
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: GNU GPLv3",
+        "Operating System :: OS Independent",
+    ],
+)
